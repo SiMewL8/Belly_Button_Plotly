a6e0539606eb9b256e7dd1c1b6efc4734ba74d56
SiMewL8 <64818157+SiMewL8@users.noreply.github.com>
2020-09-03 18:26:05 -0700

    finished module

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Belly_Button_Plotly
-Helping Roza with plotting belly button bacteria samples and creating a dashboard accessed by user input.
+
+Helping Roza with plotting belly button bacteria samples and creating a live dashboard accessed by user input.
+
+Link: https://simewl8.github.io/Belly_Button_Plotly/
+
+Roza wanted to utilize her Belly Button data samples and program a functioning website where users, with anonymized IDs, could looks up their unique data to find the probable match for the bacteria used in the Improbable Beef Burger.  
+
+Roza learned to create basic plots with Plotly Javascript library, including bar charts, line charts, and pie charts, while using D3.json() to fetch external data. Next, she practiced functional programming in JavaScript to manipulate data and increase her js knowledge. One of the unique features is an interactive data visualization that uses event handlers in JavaScript.
+
+
+One of the advantages of programming in JavaScript is that the web browser is a tool both for development and debugging. So fortunately, Roza does not need to install any software to complete this project. She used:
+
+1. VS Code
+2. A Functioning Web Browser like Google Chrome
+3. Plotly and Document Driver Document Javascript Libraries
+4. Github Live Pages 
+
+Her result is a live website with interactive data visualizations.
+
+
+### Site Screenshot
+![htmlimg](https://github.com/SiMewL8/Belly_Button_Plotly/blob/master/static/images/plotly_dashboard.jpg?raw=true)
---
+++ static/js/plots.js
@@ -0,0 +1,186 @@
+// ID selector
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("data/samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+init();
+
+
+// Base function refresh
+function optionChanged (newSample) {
+    console.log(newSample);
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+
+// Script: Filling out Demographic Info
+function buildMetadata(sample) {
+    d3.json("data/samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+
+        PANEL.html("");
+        Object.entries(result).forEach(([x,y]) => {
+            PANEL.append("h6").text((x + " : "+ y).toUpperCase());
+        })
+
+        // belly button washing frequency for gauge chart
+        var wfreqValue = result.wfreq;
+
+        // gauge chart 
+        var data = [
+            {
+              domain: { 
+                x: [0,1],
+                y: [0,1]
+              },
+              title: {
+                text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+                font:{
+                  color: "darkgreen"
+                }
+              },
+              type: "indicator",
+              mode: "gauge+number",
+              value: wfreqValue,
+              gauge: {
+                shape: "angular",
+                bar: {
+                  color: "plum",
+                  thickness: .2
+                  },
+                // bgcolor: "greenyellow",
+                axis: {
+                  range: [null,10],
+                  tickmode: "linear",
+                  tick0: 0,
+                  dtick: 1,
+                  ticklen: 8,
+                  tickcolor: "red", 
+                  },
+                steps:[
+                  {range:[0,1], color: 'rgba(14, 127, 0, .1)'},
+                  {range:[1,2], color: 'rgba(14, 127, 0, .2)'},
+                  {range:[2,3], color: 'rgba(14, 127, 0, .3)'},
+                  {range:[3,4], color: 'rgba(14, 127, 0, .4)'},
+                  {range:[4,5], color: 'rgba(14, 127, 0, .5)'},
+                  {range:[5,6], color: 'rgba(14, 127, 0, .6)'},
+                  {range:[6,7], color: 'rgba(14, 127, 0, .7)'},
+                  {range:[7,8], color: 'rgba(14, 127, 0, .8)'},
+                  {range:[8,9], color: 'rgba(14, 127, 0, .9)'},
+                  {range:[9,10], color: 'rgba(14, 127, 0, 1)'}
+                ],
+              }
+            }
+          ];
+                    
+            var layout = { width: 500, height: 350, margin: { t: 0, b: 0 } };
+        
+        Plotly.newPlot('gauge', data, layout);
+
+    })
+};
+
+
+
+function buildCharts(sample) {
+
+    // read samples.json and then (data)
+    d3.json("data/samples.json").then((data) => {
+        
+        // filter for data.samples.id
+        var dataSamples = data.samples;
+        var chosenID = dataSamples.filter(selcID => selcID.id == sample);
+        
+        // id from dropdown matches filtered id from samples array
+        // first item in the array  selected and assigned new var
+        var resultSampleID = chosenID[0];
+
+        // assiging var so d3 can find bar chart id in html
+        var TagHTML = d3.select("#bar");
+
+        // refreshed data when another id is selected from dropdown
+        TagHTML.html("");
+
+        // variables for bar and bubble charts
+        var uniqueID = resultSampleID.id;
+
+        var sampleValues = resultSampleID.sample_values;
+
+        var otuID = resultSampleID.otu_ids;
+
+        var otuLables = resultSampleID.otu_labels;
+
+        // bar chart
+        var trace1 = {
+            x: (Object.values((sampleValues.slice(0,10)).sort((a,b)=> a-b))
+                                .map(num => parseInt(num))),
+            y: (Object.values((otuID.slice(0,10)).reverse())
+                                .map(str => String('OTU ' + str))),
+            mode: 'markers',
+            type: 'bar',
+            name: 'bacterium IDs',
+            text: ((otuLables.slice(0,10)).sort((a,b)=> a-b)),
+            orientation:'h'
+          };
+          
+        var data = [ trace1 ];
+          
+        var layout = {
+            xaxis: {
+              title: 'Count of Unique Bacteria',
+            },
+            yaxis: {
+              title: 'Specific Bacteria by OTU ID',
+              standoff: 60,
+              type: 'category'
+            },
+            title:('ID '+ uniqueID +' Top Bacterial Species')
+          };
+          
+        Plotly.newPlot('bar', data, layout);
+
+        // bubble chart
+        var trace1 = {
+            x: otuID,
+            y: sampleValues,
+            text: otuLables,
+            mode: 'markers',
+            marker: {
+              color: otuID,
+              size: (Object.values(sampleValues.map(num => parseInt(num)))),
+              sizeref: 2.0 * Math.max(...sampleValues) / (20**2),
+              sizemode: 'diameter'
+            }
+          };
+          
+        var data = [trace1];
+          
+        var layout = {
+            xaxis: {
+                title: 'Bacterial Species by OTU IDs',
+              },
+            yaxis: {
+                title: 'Amount of Bacteria',
+              },
+            title: ('ID ' + uniqueID + ' Relative Frequency of Bacterial Species'),
+            showlegend: false,
+            height: 600,
+            width: 1200
+          };
+          
+        Plotly.newPlot('bubble', data, layout);
+    });
+};
